486bc69503b29cd9344851872a89371850888a4e
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-14 15:30:25 -0300

    Deplou build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/App.css";
 import { Header } from "./components/header/Header";
 import { HeroSection } from "./components/hero_section/HeroSection";
 import { ServicesSection } from "./components/services_section/ServicesSection";
